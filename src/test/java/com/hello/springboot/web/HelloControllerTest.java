a11b33c16ebfb20c650c22a4f07f0c6fd5cccb03
Giyoon Park <workspaceforict@gmail.com>
2021-10-01 03:09:07 +0900

    Add: Hello function test

---
+++ src/test/java/com/hello/springboot/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.hello.springboot.web;
+
+import com.hello.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void myHello() throws Exception{
+        String hello = "Hello!";
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+
+    }
+}
