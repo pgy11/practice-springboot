a8932c59db80e2934da956c9b6ce92307e0d5023
Giyoon Park <workspaceforict@gmail.com>
2021-10-03 00:13:26 +0900

    Update: BaseTimeEntity test added

--- src/test/java/com/hello/springboot/web/domain/posts/PostRepositoryTest.java
+++ src/test/java/com/hello/springboot/web/domain/posts/PostRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -41,5 +42,28 @@ public class PostRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+
+        // given
+        LocalDateTime now = LocalDateTime.of(2021,10,2,0,0,0);
+        postRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(
+                ">>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate()
+        );
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
