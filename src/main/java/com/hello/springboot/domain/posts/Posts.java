1bf5cd5c5dd160798192401e4cf29abba9861d12
Giyoon Park <workspaceforict@gmail.com>
2021-10-03 00:10:36 +0900

    Update: update added

--- src/main/java/com/hello/springboot/domain/posts/Posts.java
+++ src/main/java/com/hello/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.hello.springboot.domain.posts;
 
+import com.hello.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity // 테이블과 매핑되는 개체, 테이블 명은 클래스명의 언더스코어 명
-public class Posts {
+public class Posts extends BaseTimeEntity {
     
     @Id// 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 적용
@@ -34,6 +35,15 @@ public class Posts {
         this.author = author;
     }
 
-    
-    
+    /*
+    * DB에 update 쿼리를 날리지 않아도 수행되는 이유는 JPA 영속성 컨텍스트때문이다.
+    * 영속성 컨텍스트는 엔티티를 영구적으로 저장하는 환경
+    * 트랜잭션 속에서 DB로부터 데이터를 가져오면 해당 데이터는 영속성 컨텍스트가 유지됨(단, JPA엔티티 매니저가 활성화 상태)
+    * 트랜잭션이 끝나는 시점에 테이블에 변경된 데이터가 반영
+    * 위의 개념을 더티 체킹이라고 한다.
+    * */
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
